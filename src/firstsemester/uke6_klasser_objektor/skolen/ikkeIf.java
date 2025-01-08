49e4dea1a49c452f2172565144e3b16f1fea317b
gulgulyil <72104981+gulgulyil@users.noreply.github.com>
2025-01-08 22:16:23 +0100

    added others file

---
+++ src/firstsemester/uke6_klasser_objektor/skolen/ikkeIf.java
@@ -0,0 +1,17 @@
+package firstsemester.uke6_klasser_objektor.skolen;
+
+public class ikkeIf {
+    public static void main(String[] args) {
+        int alder = 13;
+
+        if(alder>=13 && alder<= 18){
+            System.out.println("Du er tenåring");
+        }
+
+        else if(alder!=18){
+            System.out.println("Du er ikke 18 år");
+        }
+    }
+
+
+}
